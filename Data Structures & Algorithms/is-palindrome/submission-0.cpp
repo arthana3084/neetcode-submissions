ff7ff4e3cafd44434aefcd724d79ab0aae178515
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-05-23 22:05:42 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        int left=0;
+        int right=s.size()-1;
+        while(left<right)
+        {
+            while(left<right && !isalnum(s[left]))
+            {
+                left++;
+            }
+            while(left<right && !isalnum(s[right]))
+            {
+                right--;
+            }
+            if(s[left]!=s[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
