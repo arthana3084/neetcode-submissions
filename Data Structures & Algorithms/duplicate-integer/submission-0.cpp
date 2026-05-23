b04bd8cc0ae3569105b1430132aea2d957f443e9
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-05-23 22:03:56 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        map<int,int> mpp;
+        for(auto it:nums)
+        {
+            mpp[it]++;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second>1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+        {
+                return false;
+        }
+        map<char,int> mpp;
+        for(auto it:s)
+        {
+            mpp[it]++;
+        }
+        for(auto it:t)
+        {
+            mpp[it]--;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
