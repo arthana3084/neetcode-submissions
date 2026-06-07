8d0f5d2917ab921ed8b2f47a6f2287da3ee7ec54
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-06-07 12:57:39 +0530

    Add: lowest-common-ancestor-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree/submission-2.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool search(TreeNode* root,TreeNode* p)
+    {
+        if(root==nullptr)
+        {
+            return false;
+        }
+        if(root->val==p->val)
+        {
+            return true;
+        }
+        bool left=search(root->left,p);
+        bool right=search(root->right,p);
+        return left or right;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* ans;
+        if(root==nullptr)
+        {
+            return nullptr;
+        }
+        bool leftp=search(root->left,p);
+        bool rightp=search(root->right,p);
+        bool leftq=search(root->left,q);
+        bool rightq=search(root->right,q);
+        if((leftp and rightq) or (leftq and rightp))
+        {
+            return root;
+        }
+        if(leftp and leftq)
+        {
+            ans=lowestCommonAncestor(root->left,p,q);
+        }
+        if(rightp and rightq)
+        {
+            ans=lowestCommonAncestor(root->right,p,q);
+        }
+        if(((leftp and !rightq) or (!leftq and rightp)) and root->val==q->val)
+        {
+            return root;
+        }
+        if(((!leftp and rightq) or (leftq and !rightp)) and root->val==p->val)
+        {
+            return root;
+        }
+        return ans;
+    }
+};
