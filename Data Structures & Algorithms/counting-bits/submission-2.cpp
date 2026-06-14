cafb30ab2c7efd6f85e3c6e1d3e24bd0ec09d399
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-06-14 22:09:31 +0530

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+    public:
+        vector<int> countBits(int n) {
+                vector<int> result;
+                        for(int i=0;i<=n;i++)
+                                {
+                                            int count=0;
+                                                        int num=i;
+                                                                    while(num)
+                                                                                {
+                                                                                                count+=(num&1);
+                                                                                                                num=num>>1;
+                                                                                                                            }
+                                                                                                                                        result.push_back(count);
+                                                                                                                                                }
+                                                                                                                                                        return result;
+                                                                                                                                                            }
+                                                                                                                                                            };
