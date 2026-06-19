615419089ab92fffb3d87fe6e67288b68e4e914c
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-06-19 13:45:18 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2=1;
+        int prev1=2;
+        if(n==1)
+        {
+            return prev2;
+        }
+        if(n==2)
+        {
+            return prev1;
+        }
+        for(int i=3;i<=n;i++)
+        {
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
