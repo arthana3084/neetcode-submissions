7fc0c74f399f486ecfd3eb1beff87d4a31c81c25
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-06-21 22:45:06 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int robhouse(vector<int>& nums,int house,vector<int>& dp)
+    {
+        if(dp[house]!=-1)
+        {
+            return dp[house];
+        }
+        dp[house]=max(nums[house]+robhouse(nums,house-2,dp),robhouse(nums,house-1,dp));
+        return max(nums[house]+robhouse(nums,house-2,dp),robhouse(nums,house-1,dp));
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        if(nums.size()==1)
+        {
+            return nums[0];
+        }
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        return robhouse(nums,nums.size()-1,dp);
+    }
+};
