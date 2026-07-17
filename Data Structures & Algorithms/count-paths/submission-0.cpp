f44f9b7bcd90d2e74e729f16ff25554ff4e9aa08
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-17 21:38:19 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int col=1;col<n;col++)
+        {
+            dp[0][col]=dp[0][col-1];
+        }
+        for(int row=1;row<m;row++)
+        {
+            dp[row][0]=dp[row-1][0];
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
